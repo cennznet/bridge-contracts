8de3b8febe58f213b499f86e0d1df3d9f3d55a2e
holygits <beauchjord@gmail.com>
2021-09-16 13:52:00 +1200

    Add tests for eth deposit

--- test/peg.test.ts
+++ test/peg.test.ts
@@ -13,10 +13,8 @@ describe('Erc20Peg', () => {
   let bridge: Contract;
   let erc20Peg: Contract;
   let testToken: Contract;
-  let provider: Provider;
 
   beforeEach(async () => {
-    provider = ethers.getDefaultProvider();
     testToken = await deployContract(wallet, TestToken, [1000000]);
     bridge = await deployContract(wallet, CENNZnetBridge, []);
     erc20Peg = await deployContract(wallet, ERC20Peg, [bridge.address]);
@@ -45,7 +43,8 @@ describe('Erc20Peg', () => {
 
   it('erc20 deposit', async () => {
     let depositAmount = 7;
-    let cennznetAddress = "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10";
+    let cennznetAddress = '0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10';
+    let userBalanceStart = await testToken.balanceOf(wallet.address);
     await erc20Peg.activateDeposits();
     await testToken.approve(erc20Peg.address, depositAmount);
 
@@ -55,55 +54,56 @@ describe('Erc20Peg', () => {
 
     // Check peg contract has funds
     expect(await testToken.balanceOf(erc20Peg.address)).to.equal(depositAmount);
+    let userBalanceEnd = await testToken.balanceOf(wallet.address);
+    expect(userBalanceEnd).to.equal(userBalanceStart - depositAmount);
   });
 
   it('native eth deposit', async () => {
-    let depositAmount = 7;
-    let cennznetAddress = "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10";
+    let depositAmount = 12345;
+    let cennznetAddress = '0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10';
     await erc20Peg.activateDeposits();
-    await testToken.approve(erc20Peg.address, depositAmount);
+    let ethTokenAddress = await erc20Peg.ETH_RESERVED_TOKEN_ADDRESS();
+    expect(ethTokenAddress).to.equal('0x0000000000000000000000000000000000000000');
+
+    let userEthStart = await erc20Peg.provider.getBalance(wallet.address);
 
     await expect(
-      erc20Peg.deposit(testToken.address, depositAmount, cennznetAddress)
-    ).to.emit(erc20Peg, 'Deposit').withArgs(wallet.address, testToken.address, depositAmount, cennznetAddress);
+      erc20Peg.deposit(ethTokenAddress, depositAmount, cennznetAddress, { value: depositAmount })
+    ).to.emit(erc20Peg, 'Deposit').withArgs(wallet.address, ethTokenAddress, depositAmount, cennznetAddress);
 
-    // Check peg contract has funds
-    expect(await provider.getBalance(erc20Peg.address)).to.equal(depositAmount);
+    // Check peg contract has eth
+    let pegEthBalance = await erc20Peg.provider.getBalance(erc20Peg.address);
+    expect(pegEthBalance).to.equal(depositAmount);
+    let userEthEnd = await erc20Peg.provider.getBalance(wallet.address);
+    // final balance is start - deposit amount - gas fees
+    expect(userEthEnd.toNumber() < (userEthStart.toNumber() - depositAmount));
   });
 
   it('deposit, peg inactive', async () => {
     await testToken.approve(erc20Peg.address, 7);
 
     await expect(
-      erc20Peg.deposit(testToken.address, 7, "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10")
+      erc20Peg.deposit(testToken.address, 7, '0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10')
     ).to.revertedWith('deposits paused');
   });
 
   it('native eth deposit invalid amount', async () => {
-    let depositAmount = '700000000000';
-    let cennznetAddress = "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10";
+    let depositAmount = 7777;
+    let cennznetAddress = '0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10';
     await erc20Peg.activateDeposits();
-    await testToken.approve(erc20Peg.address, depositAmount);
+    let ethTokenAddress = await erc20Peg.ETH_RESERVED_TOKEN_ADDRESS();
 
     await expect(
-      erc20Peg.deposit(testToken.address, depositAmount, cennznetAddress)
-    ).to.emit(erc20Peg, 'Deposit').withArgs(wallet.address, testToken.address, depositAmount, cennznetAddress);
+      erc20Peg.deposit(ethTokenAddress, depositAmount, cennznetAddress, { value: depositAmount - 1 })
+    ).to.revertedWith('incorrect deposit amount');
   });
 
-  it('deposit, no approval', async () => {
+  it('erc20 deposit, no approval', async () => {
     await erc20Peg.activateDeposits();
 
     await expect(
-      erc20Peg.deposit(testToken.address, 7, "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10")
+      erc20Peg.deposit(testToken.address, 7, '0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10')
     ).to.be.reverted;
-  });
-
-  it('deposit, zero amount', async () => {
-    await erc20Peg.activateDeposits();
-
-    await expect(
-      erc20Peg.deposit(testToken.address, 0, "0xacd6118e217e552ba801f7aa8a934ea6a300a5b394e7c3f42cd9d6dd9a457c10")
-    ).to.revertedWith('0 deposit');
   });
 
 });
