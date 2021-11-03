3964a70020380c264fb097282a3f59ed2b9c2400
Jordan <beauchjord@gmail.com>
2021-11-04 09:51:44 +1300

    update readme (#16)
    * update readme
    add rinkeby to config
    
    * fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,10 @@ module.exports = {
     }
   },
   networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.RINKEBY_INFURA_API_KEY}`,
+      accounts: [`0x${process.env.ACCOUNT_KEY}`],
+    },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ROPSTEN_INFURA_API_KEY}`,
       accounts: [`0x${process.env.ACCOUNT_KEY}`],
