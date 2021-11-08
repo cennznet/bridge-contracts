23af027dac1d21e442c1c1a3ec8129120ebc5640
KarishmaBothara <bothara.karishma@gmail.com>
2021-11-09 11:27:18 +1300

    as per review comments

--- src/api/routes/bridgeClaim.js
+++ src/api/routes/bridgeClaim.js
@@ -3,7 +3,7 @@ const { BRIDGE_CLAIM } = require('../../mongo/models');
 async function routes (fastify) {
     const collectionBridgeClaim = fastify.mongo.db.collection(BRIDGE_CLAIM);
 
-    fastify.get('/bridge/txHash/:txHash', async (request) => {
+    fastify.get('/transactions/:txHash', async (request) => {
       const result = await collectionBridgeClaim.findOne({ txHash: request.params.txHash });
       if (!result) {
         throw new Error('Record not found')
