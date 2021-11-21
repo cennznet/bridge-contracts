c5db67935199bcede35571010135e2420a8246dd
holygits <beauchjord@gmail.com>
2021-11-22 12:50:06 +1300

    Adding success case test

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -9,9 +9,9 @@ contract Timelock {
     event NewAdmin(address indexed newAdmin);
     event NewPendingAdmin(address indexed newPendingAdmin);
     event NewDelay(uint indexed newDelay);
-    event CancelTransaction(bytes32 indexed txHash, address indexed target, uint value, bytes data, uint eta);
-    event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint value, bytes data, uint eta);
-    event QueueTransaction(bytes32 indexed txHash, address indexed target, uint value, bytes data, uint eta);
+    event CancelTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature,  bytes data, uint eta);
+    event ExecuteTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature,  bytes data, uint eta);
+    event QueueTransaction(bytes32 indexed txHash, address indexed target, uint value, string signature, bytes data, uint eta);
 
     uint public constant GRACE_PERIOD = 14 days;
     uint public constant MINIMUM_DELAY = 2 days;
@@ -23,6 +23,7 @@ contract Timelock {
 
     mapping (bytes32 => bool) public queuedTransactions;
 
+
     constructor(address admin_, uint delay_) {
         require(delay_ >= MINIMUM_DELAY, "Timelock::constructor: Delay must exceed minimum delay.");
         require(delay_ <= MAXIMUM_DELAY, "Timelock::setDelay: Delay must not exceed maximum delay.");
@@ -31,8 +32,7 @@ contract Timelock {
         delay = delay_;
     }
 
-    fallback() external payable {}
-    receive() external payable {}
+    receive() external payable { }
 
     function setDelay(uint delay_) public {
         require(msg.sender == address(this), "Timelock::setDelay: Call must come from Timelock.");
@@ -58,41 +58,49 @@ contract Timelock {
         emit NewPendingAdmin(pendingAdmin);
     }
 
-    function queueTransaction(address target, uint value, bytes memory data, uint eta) public returns (bytes32) {
+    function queueTransaction(address target, uint value, string memory signature, bytes memory data, uint eta) public returns (bytes32) {
         require(msg.sender == admin, "Timelock::queueTransaction: Call must come from admin.");
         require(eta >= getBlockTimestamp().add(delay), "Timelock::queueTransaction: Estimated execution block must satisfy delay.");
 
-        bytes32 txHash = keccak256(abi.encode(target, value, data, eta));
+        bytes32 txHash = keccak256(abi.encode(target, value, signature, data, eta));
         queuedTransactions[txHash] = true;
 
-        emit QueueTransaction(txHash, target, value, data, eta);
+        emit QueueTransaction(txHash, target, value, signature, data, eta);
         return txHash;
     }
 
-    function cancelTransaction(address target, uint value, bytes memory data, uint eta) public {
+    function cancelTransaction(address target, uint value, string memory signature, bytes memory data, uint eta) public {
         require(msg.sender == admin, "Timelock::cancelTransaction: Call must come from admin.");
 
-        bytes32 txHash = keccak256(abi.encode(target, value, data, eta));
+        bytes32 txHash = keccak256(abi.encode(target, value, signature, data, eta));
         queuedTransactions[txHash] = false;
 
-        emit CancelTransaction(txHash, target, value, data, eta);
+        emit CancelTransaction(txHash, target, value, signature, data, eta);
     }
 
-    function executeTransaction(address target, uint value, bytes memory data, uint eta) public payable returns (bytes memory) {
+    function executeTransaction(address target, uint value, string memory signature, bytes memory data, uint eta) public payable returns (bytes memory) {
         require(msg.sender == admin, "Timelock::executeTransaction: Call must come from admin.");
 
-        bytes32 txHash = keccak256(abi.encode(target, value, data, eta));
+        bytes32 txHash = keccak256(abi.encode(target, value, signature, data, eta));
         require(queuedTransactions[txHash], "Timelock::executeTransaction: Transaction hasn't been queued.");
         require(getBlockTimestamp() >= eta, "Timelock::executeTransaction: Transaction hasn't surpassed time lock.");
         require(getBlockTimestamp() <= eta.add(GRACE_PERIOD), "Timelock::executeTransaction: Transaction is stale.");
 
         queuedTransactions[txHash] = false;
 
+        bytes memory callData;
+
+        if (bytes(signature).length == 0) {
+            callData = data;
+        } else {
+            callData = abi.encodePacked(bytes4(keccak256(bytes(signature))), data);
+        }
+
         // solium-disable-next-line security/no-call-value
-        (bool success, bytes memory returnData) = target.call{ value: value }(data);
+        (bool success, bytes memory returnData) = target.call{ value: value }(callData);
         require(success, "Timelock::executeTransaction: Transaction execution reverted.");
 
-        emit ExecuteTransaction(txHash, target, value, data, eta);
+        emit ExecuteTransaction(txHash, target, value, signature, data, eta);
 
         return returnData;
     }
--- test/timelock.test.ts
+++ test/timelock.test.ts
@@ -1,24 +1,57 @@
 import { expect, use } from 'chai';
-import { Contract, utils } from 'ethers';
+import hre from 'hardhat';
+import { Contract, ethers, utils } from 'ethers';
+import { BigNumber } from 'bignumber.js';
 import { deployContract, MockProvider, solidity } from 'ethereum-waffle';
 import Timelock from '../artifacts/contracts/Timelock.sol/Timelock.json';
 import CENNZnetBridge from '../artifacts/contracts/CENNZnetBridge.sol/CENNZnetBridge.json';
+import { keccak256 } from '@ethersproject/keccak256';
 
 use(solidity);
+
+async function setTime(seconds: BigNumber) {
+  await hre.network.provider.send('evm_increaseTime', [seconds.toString()]);
+  await hre.network.provider.send('evm_mine', []);
+}
 
 describe('Timelock', () => {
   const [wallet, walletTo] = new MockProvider().getWallets();
   let bridge: Contract;
   let timeLock: Contract;
+  let threeDays = new BigNumber(86400 * 3);
+  let minimumDelay = threeDays;
+  // this value is arbitrary
+  // the block timestamp will be manipulated in these tests relative to this
+  let initialBlockTimestamp = new BigNumber(100);
+  let abi = new ethers.utils.AbiCoder();
 
   beforeEach(async () => {
-    timeLock = await deployContract(wallet, Timelock, []);
+    timeLock = await deployContract(wallet, Timelock, [wallet.address, minimumDelay.toString()]);
     bridge = await deployContract(wallet, CENNZnetBridge, []);
+    await setTime(initialBlockTimestamp);
     await bridge.transferOwnership(timeLock.address);
   });
 
   it('issues a function w delay', async () => {
-    
+    let delay = minimumDelay;
+    let newMaxRewardPayout = new BigNumber('12345789');
+    let signature = 'setMaxRewardPayout(uint)';
+    let encodedParams = abi.encode(['uint'], [newMaxRewardPayout.toString()]);
+    let eta = initialBlockTimestamp.plus(delay).plus(new BigNumber(1));
+
+    let queuedTxHash = keccak256(
+      abi.encode(
+        ['address', 'uint256', 'string', 'bytes', 'uint256'],
+        [bridge.address, 0, signature, encodedParams, eta.toString()]
+      )
+    );
+
+    await timeLock.queueTransaction(bridge.address, 0, signature, encodedParams, eta.toString());
+    await setTime(delay.plus(1));
+    await timeLock.executeTransaction(bridge.address, 0, signature, encodedParams, eta.toString());
+
+    await expect(bridge.maxRewardPayout === newMaxRewardPayout);
   });
 
 })
+;
