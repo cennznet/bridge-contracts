1da29e2a98293269b1a0feccc3414560ee053035
Jason <jason.j.tulp@gmail.com>
2022-03-02 13:25:15 +1300

    Changed WCENNZ contract to remove ERC20 restrictions

--- contracts/WrappedCENNZ.sol
+++ contracts/WrappedCENNZ.sol
@@ -6,25 +6,34 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract WrappedCENNZ is ERC20 {
     // CENNZ ERC20 contract address
     address public pegAddress;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
 
     constructor(address _pegAddress) ERC20("Wrapped CENNZ", "WCENNZ") {
         pegAddress = _pegAddress;
     }
 
     function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
-        require(msg.sender == pegAddress);
-        require(owner != buyer);
-        require(buyer == pegAddress);
+        if (msg.sender == pegAddress) {
+            _burn(owner, numTokens);
+        } else {
+            _transfer(owner, buyer, numTokens);
 
-        _burn(owner, numTokens);
-
+            uint256 currentAllowance = _allowances[owner][_msgSender()];
+            require(currentAllowance >= numTokens, "ERC20: transfer amount exceeds allowance");
+            unchecked {
+                _approve(owner, _msgSender(), currentAllowance - numTokens);
+            }
+        }
         return true;
     }
 
     function transfer(address buyer, uint256 numTokens) public override returns (bool) {
-        require(msg.sender == pegAddress);
-        _mint(buyer, numTokens);
-
+        if (msg.sender == pegAddress) {
+            _mint(buyer, numTokens);
+        } else {
+            _transfer(msg.sender, buyer, numTokens);
+        }
         return true;
     }
 }
